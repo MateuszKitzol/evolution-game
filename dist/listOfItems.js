7c8016f687deddfa2c3578f74e363946b8ece590
MateuszKitzol <mateusz@kitzol.pl>
2023-09-18 21:18:45 +0200

    Try-5

---
+++ dist/listOfItems.js
@@ -0,0 +1,23 @@
+export const listOfItems = [
+    'electron',
+    'neutron',
+    'proton',
+    'carbon',
+    'hydrogen',
+    'nitrogen',
+    'oxygen',
+    'adenine',
+    'cytosine',
+    'guanine',
+    'thymine',
+    'dna',
+    'undiscovered',
+    'undiscovered',
+    'undiscovered'
+];
+
+export const listOfInitialItems = [
+    'electron',
+    'neutron',
+    'proton'
+]
