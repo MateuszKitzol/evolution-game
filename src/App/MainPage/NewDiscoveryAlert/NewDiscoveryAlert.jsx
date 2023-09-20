9cffa4948239b012569014a7b4086f8cda2f98aa
MateuszKitzol <mateusz@kitzol.pl>
2023-09-20 14:29:33 +0200

    Make images working on github pages (try 2)

--- src/App/MainPage/NewDiscoveryAlert/NewDiscoveryAlert.jsx
+++ src/App/MainPage/NewDiscoveryAlert/NewDiscoveryAlert.jsx
@@ -1,16 +1,13 @@
 import React, { useState, UseContext, useEffect } from 'react';
-import { getImagePath } from '../Sidebar/Items/ItemStack/Item/Item.jsx';
 import * as S from './styles.js'
 
 export const NewDiscoveryAlert = ({name, itemsAmountsStateChanger}) => {
-
-    const path = getImagePath(name);
     
     useEffect(() => {
         itemsAmountsStateChanger(items => items.map(x => { return {name: x.name, amount: 0}}));
     }, []);
 
     return (  
-        <S.NewDiscoveryAlert key={name} name={name} path={path}/>
+        <S.NewDiscoveryAlert key={name} name={name}/>
     )
 }
