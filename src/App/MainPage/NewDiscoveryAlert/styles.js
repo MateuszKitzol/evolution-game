435069c25f538936245ce41aa1467a8cb57e5423
MateuszKitzol <mateusz@kitzol.pl>
2023-09-20 15:52:54 +0200

    Make new discovery images working on github pages

--- src/App/MainPage/NewDiscoveryAlert/styles.js
+++ src/App/MainPage/NewDiscoveryAlert/styles.js
@@ -1,7 +1,40 @@
 import styled from 'styled-components';
+import adenineImage from '../../../../public/items/adenine.jpg';
+import carbonImage from '../../../../public/items/carbon.jpg';
+import cytosineImage from '../../../../public/items/cytosine.jpg';
+import dnaImage from '../../../../public/items/dna.jpg';
+import electronImage from '../../../../public/items/electron.jpg';
+import guanineImage from '../../../../public/items/guanine.jpg';
+import hydrogenImage from '../../../../public/items/hydrogen.jpg';
+import neutronImage from '../../../../public/items/neutron.jpg';
+import nitrogenImage from '../../../../public/items/nitrogen.jpg';
+import oxygenImage from '../../../../public/items/oxygen.jpg';
+import protonImage from '../../../../public/items/proton.jpg';
+import thymineImage from '../../../../public/items/thymine.jpg';
+import undiscoveredImage from '../../../../public/items/undiscovered.jpg';
+
+
+
+const importedImages = {
+    adenine: adenineImage, 
+    carbon: carbonImage,
+    cytosine: cytosineImage,
+    dna: dnaImage,
+    electron: electronImage,
+    guanine: guanineImage,
+    hydrogen: hydrogenImage,
+    neutron: neutronImage,
+    nitrogen: nitrogenImage,
+    oxygen: oxygenImage,
+    proton: protonImage,
+    thymine: thymineImage,
+    undiscovered: undiscoveredImage
+};
+
+
 
 export const NewDiscoveryAlert = styled.div`
-    background: url(${(props) => props.path});
+    background: url(${(props) => importedImages[props.name]});
     background-size: contain; 
     border: 3px solid purple;
     border-radius: 20px;
