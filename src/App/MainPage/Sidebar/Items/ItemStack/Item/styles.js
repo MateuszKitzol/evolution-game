e8125ef94543dbe6ebca65374095c68b85e53f3c
MateuszKitzol <mateusz@kitzol.pl>
2023-12-15 18:07:47 +0100

    Add cursor animation

--- src/App/MainPage/Sidebar/Items/ItemStack/Item/styles.js
+++ src/App/MainPage/Sidebar/Items/ItemStack/Item/styles.js
@@ -13,6 +13,7 @@ import oxygenImage from '../../../../../../../public/items/oxygen.jpg';
 import protonImage from '../../../../../../../public/items/proton.jpg';
 import thymineImage from '../../../../../../../public/items/thymine.jpg';
 import undiscoveredImage from '../../../../../../../public/items/undiscovered.jpg';
+import cursorImage from '../../../../../../../public/items/cursor.png';
 
 
 
@@ -20,6 +21,7 @@ const importedImages = {
     adenine: adenineImage, 
     carbon: carbonImage,
     cytosine: cytosineImage,
+    cursor: cursorImage,
     dna: dnaImage,
     electron: electronImage,
     guanine: guanineImage,
@@ -64,6 +66,40 @@ export const Item = styled.div`
         margin: 20px;
         z-index: 100;
         visibility: ${(props) => props.$shadowed ? 'hidden' : 'visible'};
+    }
+
+    &:before {
+        background-image: url(${(props) => importedImages.cursor});
+        background-size: 60px 60px;
+        background-repeat: no-repeat;
+        content: "";
+        display: block;
+        height: 100px;
+        width: 100px;
+        cursor: ${(props) => (props.name === 'undiscovered' || props.$shadowed) ? 'default' : 'pointer'};
+        font-size: 15px;
+        position: absolute;
+        margin: 15px;
+        z-index: 100;
+        visibility: ${(props) => props.name == "proton" ? 'visible' : 'hidden'};
+        ${(props) => props.name === 'proton' && `
+            opacity: 1;
+            transition: all 2s ease 2s;
+            left: 0;
+            animation: disappear 2s forwards 2s;
+        `}
+    }
+
+    @keyframes disappear {
+        from {
+            left: 0;
+            visibility: visible;
+        }
+
+        to {
+            left: 400px;
+            visibility: hidden;
+        }
     }
 
     &:hover ${DeleteIcon} {
